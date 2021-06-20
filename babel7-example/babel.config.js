9fabf5be65ebeac7dc15b703e5296ffacca3bf28
Joao Pedro Lira Dos Santos <joao.santos@take.net>
2021-06-20 18:07:26 -0300

    Create babel7 project

---
+++ babel7-example/babel.config.js
@@ -0,0 +1,11 @@
+const presets = [
+    [
+        "@babel/preset-env",
+        {
+            useBuiltIns: "usage",
+            corejs: 3,
+        }
+    ]
+]
+
+module.exports = { presets }
---
+++ babel7-example/src/es6.js
@@ -0,0 +1,4 @@
+var a = new Map()
+var b = Promise.resolve();
+Object.assign({}, {1: 1});
+"foobar".includes('foo');
